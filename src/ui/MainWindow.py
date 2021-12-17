28f919000c8d2a149180c4c1a53fc419038aeb17
Jan Hettenkofer <jan@hettenkofer.net>
2021-12-18 00:15:31 +0100

    Add histogram & colour control for highlights and shadows, reorganise UI

--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -4,11 +4,11 @@
 
 from typing import Optional
 from PySide6.QtCore import QRect, Qt
-from PySide6.QtWidgets import QDockWidget, QGridLayout, QGroupBox, QLabel, QSlider, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QDockWidget, QGridLayout, QGroupBox, QLabel, QSlider, QDoubleSpinBox, QVBoxLayout, QWidget
 from PySide6.QtOpenGLWidgets import QOpenGLWidget
 from PySide6.QtGui import QColor, QImage, QPaintEvent, QPainter
 from numpy.lib.type_check import imag
-from superqt import QRangeSlider
+from superqt import QRangeSlider, QDoubleSlider
 import numpy as np
 
 import rawpy
@@ -19,45 +19,131 @@ class LabelledSlider(QWidget):
         super().__init__(parent=parent, f=f)
 
         self.label = QLabel(label, self)
-        self.slider = QSlider(Qt.Horizontal, self)
+        self.spinbox = QDoubleSpinBox(self)
+        self.spinbox.setStepType(QDoubleSpinBox.AdaptiveDecimalStepType)
+        self.spinbox.setDecimals(4)
+        self.slider = QDoubleSlider(Qt.Horizontal, self)
 
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.slider)
+        def update_spinbox():
+            self.spinbox.setValue(self.slider.value())
 
-        self.setLayout(self.layout)
+        def update_slider():
+            self.slider.setValue(self.spinbox.value())
+
+        self.slider.sliderReleased.connect(update_spinbox)
+        self.spinbox.editingFinished.connect(update_slider)
+
+        self._layout = QGridLayout(self)
+        self._layout.setColumnStretch(0, 2)
+        self._layout.setColumnStretch(1, 1)
+        self._layout.addWidget(self.label, 0, 0)
+        self._layout.addWidget(self.spinbox, 0, 1)
+        self._layout.addWidget(self.slider, 1, 0, 1, 2)
+
+        self.setLayout(self._layout)
+
+    def setValue(self, v: float) -> None:
+        self.slider.setValue(v)
+        self.spinbox.setValue(v)
+
+    def setMaximum(self, v: float) -> None:
+        self.slider.setMaximum(v)
+        self.spinbox.setMaximum(v)
+
+    def setMinimum(self, v: float) -> None:
+        self.slider.setMinimum(v)
+        self.spinbox.setMinimum(v)
 
 class PropertiesPane(QGroupBox):
-    def __init__(self, parent: Optional[QWidget] = None) -> None:
+    def __init__(self, histogram, parent: Optional[QWidget] = None) -> None:
         super().__init__("Properties", parent=parent)
 
         # self.exposure = LabelledSlider("Exposure", self)
         # self.white_balance = LabelledSlider("White Balance", self)
         # self.tint = LabelledSlider("Tint", self)
 
-        self.shadows = LabelledSlider("Shadows", self)
-        self.shadows.slider.setMinimum(-100)
-        self.shadows.slider.setMaximum(100)
-        self.shadows.slider.setValue(0)
+        self.histogram = ImageRenderer(histogram)
 
-        self.highlights = LabelledSlider("Highlights", self)
-        self.highlights.slider.setMinimum(0)
-        self.highlights.slider.setMaximum(200)
-        self.highlights.slider.setValue(100)
+        ### GAMMA ###
 
         self.gamma = LabelledSlider("Gamma", self)
-        self.gamma.slider.setMinimum(0)
-        self.gamma.slider.setMaximum(1000)
-        self.gamma.slider.setValue(100)
+        self.gamma.setMinimum(0)
+        self.gamma.setMaximum(10)
+        self.gamma.setValue(1)
+
+        ### SHADOWS ###
+
+        self.shadows_group = QGroupBox("Shadows", self)
+        self.shadows_layout = QVBoxLayout(self.shadows_group)
+        self.shadows_layout.setAlignment(Qt.AlignTop)
+
+        self.shadows = LabelledSlider("Shadows: Main", self.shadows_group)
+        self.shadows.setMinimum(-1)
+        self.shadows.setMaximum(1)
+        self.shadows.setValue(0)
+
+        self.shadow_red = LabelledSlider("Shadows: Red", self.shadows_group)
+        self.shadow_red.setMinimum(-0.3)
+        self.shadow_red.setMaximum(0.3)
+        self.shadow_red.setValue(0)
+        self.shadow_green = LabelledSlider("Shadows: Green", self.shadows_group)
+        self.shadow_green.setMinimum(-0.3)
+        self.shadow_green.setMaximum(0.3)
+        self.shadow_green.setValue(0)
+        self.shadow_blue = LabelledSlider("Shadows: Blue", self.shadows_group)
+        self.shadow_blue.setMinimum(-0.3)
+        self.shadow_blue.setMaximum(0.3)
+        self.shadow_blue.setValue(0)
+
+        self.shadows_layout.addWidget(self.shadows)
+        self.shadows_layout.addWidget(self.shadow_red)
+        self.shadows_layout.addWidget(self.shadow_green)
+        self.shadows_layout.addWidget(self.shadow_blue)
+
+        self.shadows_group.setLayout(self.shadows_layout)
+
+        ### HIGHLIGHTS ###
+
+        self.highlights_group = QGroupBox("Highlights", self)
+        self.highlights_layout = QVBoxLayout(self.shadows_group)
+        self.highlights_layout.setAlignment(Qt.AlignTop)
+
+        self.highlights = LabelledSlider("Highlights: Main", self.highlights_group)
+        self.highlights.setMinimum(0)
+        self.highlights.setMaximum(2)
+        self.highlights.setValue(1)
+
+        self.high_red = LabelledSlider("Highlights: Red", self.highlights_group)
+        self.high_red.setMinimum(0.9)
+        self.high_red.setMaximum(1.1)
+        self.high_red.setValue(1)
+        self.high_green = LabelledSlider("Highlights: Green", self.highlights_group)
+        self.high_green.setMinimum(0.9)
+        self.high_green.setMaximum(1.1)
+        self.high_green.setValue(1)
+        self.high_blue = LabelledSlider("Highlights: Blue", self.highlights_group)
+        self.high_blue.setMinimum(0.9)
+        self.high_blue.setMaximum(1.1)
+        self.high_blue.setValue(1)
+
+        self.highlights_layout.addWidget(self.highlights)
+        self.highlights_layout.addWidget(self.high_red)
+        self.highlights_layout.addWidget(self.high_green)
+        self.highlights_layout.addWidget(self.high_blue)
+
+        self.highlights_group.setLayout(self.highlights_layout)
+
+        ### GENERAL SETUP ###
 
         self._layout = QVBoxLayout(self)
         self._layout.setAlignment(Qt.AlignTop)
-        # self._layout.addWidget(self.exposure)
-        # self._layout.addWidget(self.white_balance)
-        # self._layout.addWidget(self.tint)
-        self._layout.addWidget(self.shadows)
-        self._layout.addWidget(self.highlights)
+
+        self._layout.addWidget(self.histogram)
+
         self._layout.addWidget(self.gamma)
+
+        self._layout.addWidget(self.shadows_group)
+        self._layout.addWidget(self.highlights_group)
 
         self.setLayout(self._layout)
 
@@ -119,14 +205,28 @@ class ImageRenderer(QOpenGLWidget):
 ####################################################
 
 def gamma(image, gamma):
-    return image ** gamma
+    return np.maximum(0, image) ** gamma
 
 def linear_contrast(image, shadows: float, highlights: float):
     """
     Adjust contrast linearly to parameters
     """
 
-    return image * highlights + shadows
+    slope = highlights - shadows
+
+    return image * slope + shadows
+
+def highlow_balance(image,
+    shadow_red, shadow_green, shadow_blue,
+    high_red, high_green, high_blue):
+    """
+    Adjust the balance of the highlights
+    """
+    shadows = np.array((shadow_red, shadow_green, shadow_blue))
+    highs = np.array((high_red, high_green, high_blue))
+
+    slope = highs - shadows
+    return image * slope + shadows
 
 def invert(image):
     # max_r = np.max(image[..., 0])
@@ -153,6 +253,14 @@ class Editor():
         self.shadows = 0
         self.highlights = 1
         self.gamma = 1
+
+        self.shadow_red = 0
+        self.shadow_green = 0
+        self.shadow_blue = 0
+
+        self.high_red = 1
+        self.high_green = 1
+        self.high_blue = 1
 
         # self._linear_white_balanced = np.array(self._raw_data.postprocess(
         #     user_wb=self.white_balance,
@@ -199,23 +307,35 @@ class Editor():
     def linear_contrast_adjusted(self, fit: tuple[int]) -> np.ndarray:
         return linear_contrast(self.inverted(fit), self.shadows, self.highlights)
 
+    def highlow_balance_adjusted(self, fit: tuple[int]) -> np.ndarray:
+        return highlow_balance(self.linear_contrast_adjusted(fit),
+            self.shadow_red,
+            self.shadow_green,
+            self.shadow_blue,
+            self.high_red,
+            self.high_green,
+            self.high_blue,
+            )
+
     def gamma_adjusted(self, fit: tuple[int]) -> np.ndarray:
-        return gamma(self.linear_contrast_adjusted(fit), self.gamma)
+        return gamma(self.highlow_balance_adjusted(fit), self.gamma)
 
     def process(self):
         """
         Process the image with the set parameters
         """
-        linear_white_balanced = np.array(self._raw_data.postprocess(
+        out = np.array(self._raw_data.postprocess(
             user_wb=self.white_balance,
             # output_color=rawpy.ColorSpace.ProPhoto,
             output_color=rawpy.ColorSpace.raw,
             output_bps=16
             ), dtype=np.float32) / np.iinfo(np.uint16).max
 
-        inverted = self.invert(linear_white_balanced)
+        out = invert(out)
+        out = linear_contrast(out, self.shadows, self.highlights)
+        out = gamma(out, self.gamma)
 
-        return inverted
+        return out
 
     @property
     def white_balance_sample_area(self):
@@ -227,14 +347,6 @@ class Editor():
     @white_balance_sample_area.setter
     def white_balance_sample_area(self, area: tuple[float]):
         self._white_balance_sample_area = area
-        print(f"WB area: {self._white_balance_sample_area}")
-
-        # self._linear_white_balanced = np.array(self._raw_data.postprocess(
-        #     user_wb=self.white_balance,
-        #     # output_color=rawpy.ColorSpace.ProPhoto,
-        #     output_color=rawpy.ColorSpace.raw,
-        #     output_bps=16
-        #     ), dtype=np.float32) / np.iinfo(np.uint16).max
 
     @property
     def white_balance(self):
@@ -271,13 +383,13 @@ class MainWindow(QWidget):
         # Data setup
         editor = Editor(cl_args.file)
 
-        img = editor.gamma_adjusted((1024, 1024))
+        def get_image(fit: tuple[int]) -> np.ndarray:
+            return editor.gamma_adjusted(fit)
+
+        img = get_image((1024, 1024))
 
         # UI setup
         self.setWindowTitle("Neo Alchemist")
-
-        # Properties panel
-        props = PropertiesPane(self)
 
         viewer_group = QGroupBox("Viewer", self)
         viewer_layout = QGridLayout()
@@ -286,8 +398,33 @@ class MainWindow(QWidget):
         vertical_wb_range = QRangeSlider()
         horizontal_wb_range = QRangeSlider(Qt.Horizontal)
 
+        def compute_histogram(buckets = 256, height = 128):
+            img = get_image((256, 256))
+
+            r_hist, _ = np.histogram(img[:,:,0], bins=buckets, range=(0, 1))
+            g_hist, _ = np.histogram(img[:,:,1], bins=buckets, range=(0, 1))
+            b_hist, _ = np.histogram(img[:,:,2], bins=buckets, range=(0, 1))
+
+            hist_img = np.zeros((height, 256, 3), dtype=np.float32)
+
+            max_v = np.max(r_hist)
+            max_v = max(np.max(g_hist), max_v)
+            max_v = max(np.max(b_hist), max_v)
+
+            scale = height / max_v
+            for x in range(buckets):
+                hist_img[height - int(scale * r_hist[x]):, x, 0] = 1
+                hist_img[height - int(scale * g_hist[x]):, x, 1] = 1
+                hist_img[height - int(scale * b_hist[x]):, x, 2] = 1
+
+            return hist_img
+
+        # Properties panel
+        props = PropertiesPane(compute_histogram(), self)
+
         def update_viewer():
-            viewer.image = editor.gamma_adjusted((1024, 1024))
+            viewer.image = get_image((1024, 1024))
+            props.histogram.image = compute_histogram()
 
             viewer.overlay = [QRect(
                 editor.white_balance_sample_area[0] * viewer.width,
@@ -297,6 +434,7 @@ class MainWindow(QWidget):
                 )]
 
             viewer.update()
+            props.histogram.update()
 
         def set_vertical_wb_area(values):
             current = editor.white_balance_sample_area
@@ -325,19 +463,55 @@ class MainWindow(QWidget):
         horizontal_wb_range.valueChanged.connect(set_horizontal_wb_area)
 
         def set_shadows(value):
-            editor.shadows = value / 100
+            editor.shadows = value
             update_viewer()
 
         props.shadows.slider.valueChanged.connect(set_shadows)
 
+        def set_shadow_red(value):
+            editor.shadow_red = value
+            update_viewer()
+
+        props.shadow_red.slider.valueChanged.connect(set_shadow_red)
+
+        def set_shadow_green(value):
+            editor.shadow_green = value
+            update_viewer()
+
+        props.shadow_green.slider.valueChanged.connect(set_shadow_green)
+
+        def set_shadow_blue(value):
+            editor.shadow_blue = value
+            update_viewer()
+
+        props.shadow_blue.slider.valueChanged.connect(set_shadow_blue)
+
         def set_highlights(value):
-            editor.highlights = value / 100
+            editor.highlights = value
             update_viewer()
 
         props.highlights.slider.valueChanged.connect(set_highlights)
 
+        def set_high_red(value):
+            editor.high_red = value
+            update_viewer()
+
+        props.high_red.slider.valueChanged.connect(set_high_red)
+
+        def set_high_green(value):
+            editor.high_green = value
+            update_viewer()
+
+        props.high_green.slider.valueChanged.connect(set_high_green)
+
+        def set_high_blue(value):
+            editor.high_blue = value
+            update_viewer()
+
+        props.high_blue.slider.valueChanged.connect(set_high_blue)
+
         def set_gamma(value):
-            editor.gamma = value / 100
+            editor.gamma = value
             update_viewer()
 
         props.gamma.slider.valueChanged.connect(set_gamma)
